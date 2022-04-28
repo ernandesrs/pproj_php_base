29f42cc4fd589d44dc99c99d8211ce127c866db9
Ernandes R de Souza <ernandesrsouza@gmail.com>
2022-04-28 09:53:00 -0400

    Função para fácil acesso à sessão e função para renderizar alertas da sessão

--- config/functions/helpers.php
+++ config/functions/helpers.php
@@ -1,2 +1,25 @@
 <?php
 
+/**
+ * Obtém sessão
+ * @return \App\Core\Session
+ */
+function session(): \App\Core\Session
+{
+    return (new \App\Core\Session());
+}
+
+/**
+ * Obtém e renderiza alerta armazenada na sessão
+ * @return string|null
+ */
+function flash_alert(): ?string
+{
+    $alert = session()->alert;
+    if (!$alert) return null;
+
+    /** @var \App\Core\Alert */
+    $unserializedAlert = unserialize($alert);
+
+    return $unserializedAlert->render();
+}
