1b22cef8f88c86c305a628fb1c578c30c1eb8523
Ernandes R de Souza <ernandesrsouza@gmail.com>
2022-05-03 17:31:18 -0400

    Criando Model

---
+++ app/Core/Model.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Core;
+
+use CoffeeCode\DataLayer\DataLayer;
+
+class Model extends DataLayer
+{
+    /**
+     * @param string $table
+     * @param array $required
+     * @param string $primary
+     * @param bool $timestamps
+     */
+    public function __construct(string $table, array $required = [], string $primary = "id", bool $timestamps = true)
+    {
+        parent::__construct($table, $required, $primary, $timestamps);
+    }
+}
