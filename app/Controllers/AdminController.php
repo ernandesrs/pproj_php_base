3e6abda38bfb9297780d4a396c7d3e20089b78cf
Ernandes R de Souza <ernandesrsouza@gmail.com>
2022-05-03 20:18:04 -0400

    Restringindo acesso ao administrativo

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -9,6 +9,17 @@ class AdminController extends Controller
 {
     public function __construct(Router $router)
     {
+        $auth = logged();
+        if (!$auth) {
+            header("Location: " . $router->route("front.index"));
+            return;
+        }
+
+        if (!in_array($auth->level, [2, 3])) {
+            header("Location: " . $router->route("front.index"));
+            return;
+        }
+
         parent::__construct($router);
     }
 }
